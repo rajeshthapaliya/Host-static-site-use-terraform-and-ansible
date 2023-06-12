66de0d74285abd813fd6394d240321d5b9a8be96
rajesh-thapaliya <thapaliyarajesh64@gmail.com>
2023-06-12 11:31:03 +0545

    first commit

---
+++ Host static site (use terraform and ansible)/terraform/modules/ec2/variable.tf
@@ -0,0 +1,19 @@
+variable "ami" {
+  default = ""
+}
+
+variable "instance_type" {
+  default = ""
+}
+
+variable "subnet_id" {
+ default = ""
+}
+
+variable "security_group" {
+  default = ""
+}
+
+variable "key_name" {
+  default = ""
+}
